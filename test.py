0e304652130aac1024dd0057f1725d9aaf3346b7
Steven Schlechte <96142435+StevenSchlechte@users.noreply.github.com>
2023-07-21 11:31:26 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 import yaml
-
-with open("./config2.yml", "r") as yamlfile:
-
-   data = yaml.load(yamlfile, Loader=yaml.FullLoader)
-   print(data)
+with open('config2.yaml') as f:
+    data = yaml.load(f, Loader=SafeLoader)
+    print(data)
