3d5e2f6e7d2b0c1e8b18278f63737e4c387f3ee8
Steven Schlechte <96142435+StevenSchlechte@users.noreply.github.com>
2023-07-21 12:22:38 +0200

    Update build.py

--- build.py
+++ build.py
@@ -15,6 +15,8 @@ from yaml.loader import SafeLoader
 with open('/home/runner/work/testrepoyml/testrepoyml/config.yml') as f:
     configurations = yaml.load(f, Loader=SafeLoader)
 
+print(configurations)
+
 
 
 class File(ABC):
