92da1f96266e1ea1e4e7f130c223bb1a3738df54
Steven Schlechte <96142435+StevenSchlechte@users.noreply.github.com>
2023-07-21 10:31:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("Hello World from the Devops repo!")
+import yaml
+
+with open("configuration.yaml", "r") as yamlfile:
+
+   data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+    print("Read successful")
+print(data)
