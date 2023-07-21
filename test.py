ac1723fbaf69b192f64b6a79fccd0d2de0016899
Steven Schlechte <96142435+StevenSchlechte@users.noreply.github.com>
2023-07-21 11:04:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import yaml
 with open("../configuration.yaml", "r") as yamlfile:
 
    data = yaml.load(yamlfile, Loader=yaml.FullLoader)
-    print("Read successful")
-print(data)
+       print("Read successful")
+   print(data)
