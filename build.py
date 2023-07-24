aea0a63cf19ed1dc4eabc0b20bbaf759b6aef482
Steven Schlechte <96142435+StevenSchlechte@users.noreply.github.com>
2023-07-24 10:33:29 +0200

    Update build.py

--- build.py
+++ build.py
@@ -231,7 +231,7 @@ clean_directory(distribution_path)
 
 with open('/home/runner/work/testrepoyml/testrepoyml/config.yml') as f:
      configurationsDic = yaml.load(f, Loader=yaml.SafeLoader)
-
+print(configurationsDic)
 configurations = list(configurationsDic.items())
 
 print(configurations)
