08ad6394c3e8a516a9b4c2729c551b59914b0ec1
Steven Schlechte <96142435+StevenSchlechte@users.noreply.github.com>
2023-07-25 10:26:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ setup(
     name='benchmark-example',
     version='0.0.0',
     url='https://github.com/benchmark-action/github-action-benchmark',
-    author='rhysd <https://rhysd.github.io>',
-    author_email='github@users.noreply.github.com',
     description='Benchmark example with timeit package',
     packages=['fib'],
 )
